a7c03c5fba8347b2826321204878f273415c03a3
Muhammad Ateeb Kashif <159563505+Microtechbymak26@users.noreply.github.com>
2025-07-06 23:32:12 +0500

    third commit

---
+++ agents.py
@@ -0,0 +1,4 @@
+from chainlit.input import Prompt
+from chainlit.agent import Agent, Runner
+from chainlit.input.openai import OpenAIChatCompletionsModel, AsyncOpenAI
+from chainlit.input.run_config import RunConfig
